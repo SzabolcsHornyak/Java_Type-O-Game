28a64a8076dc7d5cabb6cd07f05d77281be7c03c
SzabolcsHornyak <design85@gmail.com>
2017-08-15 13:41:49 +0200

    Add Wordscore class

--- src/com/jarheads/NewGame.java
+++ src/com/jarheads/NewGame.java
@@ -1,8 +1,12 @@
 package com.jarheads;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NewGame {
     public static void new_game() {
         // same thing but instead of exit, break the while loop
         System.out.println("New");
     }
+
 }
