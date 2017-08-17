b259304f90c419b252836303df1c46ae30609048
gergelyHorvath <gergely.horvath126@gmail.com>
2017-08-17 11:27:25 +0200

    creates GameOver class

--- src/com/jarheads/Timer.java
+++ src/com/jarheads/Timer.java
@@ -2,10 +2,10 @@ package com.jarheads;
 
 public class Timer implements Runnable{
 
-    public static int counter = 60;
+    public static int counter = 10;
 
     public void run() {
-        counter = 60;
+        counter = 10;
         while(counter > 0) {
             try {
                 Thread.sleep(1000);
